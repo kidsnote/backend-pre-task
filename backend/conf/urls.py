421c20fae5d314eec6c27735d97e35463c676c6d
jslee <dlwhdtjd098@gmail.com>
2024-08-03 13:43:10 +0900

    feat(conf.urls): healthcheck, debug toolbar 추가

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.http import JsonResponse
+from django.urls import path, include
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
 ]
+
+if settings.DEBUG:
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        urlpatterns += [
+            path(
+                "__debug__/", include("debug_toolbar.urls")
+            ),
+        ]
